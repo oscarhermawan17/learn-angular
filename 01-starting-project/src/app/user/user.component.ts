a7657f0b43ba02fccb5c9277fc461b10c5b70170
Oscar Hermawan <oscar.hermawan90@gmail.com>
2025-09-30 16:54:15 +0700

    section 2

---
+++ 01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,46 @@
+import { Component, computed, EventEmitter, Input, input, output, Output } from '@angular/core';
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  //Decorator Input, Old way to receive value from parent (props in react)
+  
+  @Input({ required: true }) user!: User
+
+  
+  @Output() select = new EventEmitter<string>()
+  
+
+  // New feature, with "signal"
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  
+  // output is not a signal, 
+  // select = output<string>()  
+
+  // With Signal, use computed
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar()
+  // }) 
+
+  // Without signal (just Input)
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id)
+  }
+}
