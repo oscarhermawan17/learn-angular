368145d2065e85a4e76e03d7254d0cbdca9855f8
Oscar Hermawan <oscar.hermawan90@gmail.com>
2025-10-08 17:00:05 +0700

    start section 4

---
+++ 03/src/app/tasks/task/task.model.ts
@@ -0,0 +1,13 @@
+export interface NewTaskData {
+  title: string;
+  summary: string;
+  date: string
+}
+
+export interface Task {
+  id: string;
+  userId: string;
+  title: string;
+  summary: string;
+  dueDate: string;
+}
---
+++ 03/src/app/user/user.component.ts
@@ -0,0 +1,42 @@
+import { Component, computed, EventEmitter, Input, input, output, Output } from '@angular/core';
+import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  //Decorator Input, Old way to receive value from parent (props in react)
+  
+  @Input({ required: true }) user!: User
+  @Input({ required: true }) selected!: boolean
+
+  @Output() select = new EventEmitter<string>()
+  
+
+  // New feature, with "signal"
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  
+  // output is not a signal, 
+  // select = output<string>()  
+
+  // With Signal, use computed
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar()
+  // }) 
+
+  // Without signal (just Input)
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id)
+  }
+}
