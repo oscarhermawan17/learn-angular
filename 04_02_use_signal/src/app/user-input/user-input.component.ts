f5f5eb954cc2d55dff305203f198bb4aeb6c91d1
Oscar Hermawan <oscar.hermawan90@gmail.com>
2025-10-10 10:25:03 +0700

    section 4 form with signal

---
+++ 04_02_use_signal/src/app/user-input/user-input.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, output, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  calculate = output<InvestmentInput>()
+
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment()
+    });
+
+    // reset form after submit
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('0');
+    this.enteredDuration.set('0');
+  }
+}
